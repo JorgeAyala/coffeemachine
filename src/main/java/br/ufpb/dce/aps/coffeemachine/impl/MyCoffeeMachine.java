1786a5ad0b454997935ced3da03923f3a2b3bba8
Aluno <me@me.com>
2014-07-29 09:40:07 -0300

    Task 1.11 finished for OO

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -73,9 +73,9 @@ public class MyCoffeeMachine implements CoffeeMachine {
 		this.factory.getCupDispenser().contains(1);
 		this.factory.getWaterDispenser().contains(anyDouble());
 		this.factory.getCoffeePowderDispenser().contains(anyDouble());
-		
-		for (int i = 0; i < this.moedas.size(); i++){
-			 devolverCash.release(this.moedas.get(i)); 
+
+		for (int i = 0; i < this.moedas.size(); i++) {
+			devolverCash.release(this.moedas.get(i));
 		}
 
 		this.factory.getDisplay().info(Messages.MIXING);
@@ -87,59 +87,10 @@ public class MyCoffeeMachine implements CoffeeMachine {
 		this.factory.getDrinkDispenser().release(0.1);
 		this.factory.getDisplay().info(Messages.TAKE_DRINK);
 
+		this.moedas.clear();
+
 		this.factory.getDisplay().info(Messages.INSERT_COINS);
 
-		moedas.clear();
-
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-	/*
-	 * public void select(Drink drink) { factory.getCupDispenser().contains(1);
-	 * factory.getWaterDispenser().contains(anyDouble());
-	 * if(factory.getCoffeePowderDispenser().contains(anyDouble())){
-	 * factory.getDisplay().warn(Messages.OUT_OF_COFFEE_POWDER);
-	 * 
-	 * for (int i = 0; i < this.moedas.size(); i++){
-	 * cb.release(this.moedas.get(i)); }
-	 * 
-	 * factory.getDisplay().info(Messages.INSERT_COINS); }
-	 * 
-	 * if (drink == Drink.BLACK_SUGAR){
-	 * factory.getSugarDispenser().contains(1.0); }
-	 * 
-	 * if (drink == Drink.BLACK_SUGAR){
-	 * factory.getSugarDispenser().release(1.0); }
-	 * 
-	 * factory.getDisplay().info(Messages.RELEASING);
-	 * factory.getCupDispenser().release(1);
-	 * factory.getDrinkDispenser().release(1.0);
-	 * factory.getDisplay().info(Messages.TAKE_DRINK);
-	 * 
-	 * factory.getWaterDispenser().release(1.0);
-	 * factory.getDisplay().info(Messages.MIXING);
-	 * 
-	 * factory.getCoffeePowderDispenser().release(1.0);
-	 * 
-	 * 
-	 * 
-	 * this.moedas.clear();
-	 * 
-	 * factory.getDisplay().info(Messages.INSERT_COINS);
-	 * 
-	 * }
-	 */
 
 }
