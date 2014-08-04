d4db67060fd4e7143f5d026ef200dd870b29a9f9
Aluno <me@me.com>
2014-08-04 08:35:01 -0300

    Task 1.12 finished for OO

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -1,12 +1,10 @@
 package br.ufpb.dce.aps.coffeemachine.impl;
 
-import static org.mockito.Matchers.anyDouble;
 import br.ufpb.dce.aps.coffeemachine.CashBox;
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachineException;
 import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
-import br.ufpb.dce.aps.coffeemachine.Dispenser;
 import br.ufpb.dce.aps.coffeemachine.Drink;
 import br.ufpb.dce.aps.coffeemachine.Messages;
 
@@ -18,8 +16,6 @@ public class MyCoffeeMachine implements CoffeeMachine {
 	private int divisao, resto;
 	CashBox devolverCash;
 	ArrayList<Coin> moedas = new ArrayList<Coin>();
-	private Drink bebida;
-	private Dispenser dispensa;
 
 	public MyCoffeeMachine(ComponentsFactory factory) {
 		this.factory = factory;
@@ -71,26 +67,50 @@ public class MyCoffeeMachine implements CoffeeMachine {
 
 	public void select(Drink drink) {
 		this.factory.getCupDispenser().contains(1);
-		this.factory.getWaterDispenser().contains(anyDouble());
-		this.factory.getCoffeePowderDispenser().contains(anyDouble());
+		this.factory.getWaterDispenser().contains(3);
+		this.factory.getCoffeePowderDispenser().contains(200);
+		
+		if(drink == Drink.BLACK_SUGAR){
 
-		for (int i = 0; i < this.moedas.size(); i++) {
-			devolverCash.release(this.moedas.get(i));
+			this.factory.getSugarDispenser().contains(200);
 		}
+		
+		this.factory.getDisplay().info("Mixing ingredients.");
+		
+					
+		this.factory.getCoffeePowderDispenser().release(200);
+		this.factory.getWaterDispenser().release(3);
+		
+		if(drink == Drink.BLACK_SUGAR){
 
-		this.factory.getDisplay().info(Messages.MIXING);
-		this.factory.getCoffeePowderDispenser().release(anyDouble());
-		this.factory.getWaterDispenser().release(anyDouble());
+			this.factory.getSugarDispenser().release(200);
+
+		}
 
 		this.factory.getDisplay().info(Messages.RELEASING);
 		this.factory.getCupDispenser().release(1);
-		this.factory.getDrinkDispenser().release(0.1);
+		this.factory.getDrinkDispenser().release(1);
 		this.factory.getDisplay().info(Messages.TAKE_DRINK);
 
-		this.moedas.clear();
+		
 
 		this.factory.getDisplay().info(Messages.INSERT_COINS);
+		
+		this.moedas.clear();
 
 	}
+	
+	
+
+
+	
+
 
 }
+		
+	
+
+
+
+
+
